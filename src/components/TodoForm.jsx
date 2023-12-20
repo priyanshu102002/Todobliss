df82ea7c0d9dd4806711f0b9a88cb537fb26a35a
Priyanshu Kumar <65441505+priyanshu102002@users.noreply.github.com>
2023-12-20 21:19:35 +0530

    Update color

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -25,7 +25,7 @@ const TodoForm = () => {
       />
       <button
         type="submit"
-        className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0"
+        className="rounded-r-lg px-3 py-1 bg-[#e76f51] text-white shrink-0"
       >
         Add
       </button>
